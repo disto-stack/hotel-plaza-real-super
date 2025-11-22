ee8b1de5a14d0ff3d92a0e3ae673c411933d6c02
disto-stack <juan.ocampo.2501@gmail.com>
2025-11-22 13:16:13 -0500

    Update seed data for rooms table with revised descriptions and configurations
    - Modified room descriptions for better clarity and accuracy.
    - Adjusted room configurations, including capacity and type for several entries.
    - Ensured no duplicates on re-seeding by implementing ON CONFLICT clause.

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -26,16 +26,17 @@ INSERT INTO public.rooms (
     ('208', 'double', 2, 2, 30000.00, 25000.00, 10000.00, 'available', 'Habitación con cama doble', ARRAY['Baño privado']),
     
     -- Third floor
-    ('302', 'familiar', 3, 3, 30000.00, 25000.00, 20000.00, 'available', 'Habitación familiar con cama individual, doble y camarotes', ARRAY['Baño privado']),
-    ('303', 'familiar', 3, 6, 30000.00, 25000.00, 20000.00, 'available', 'Habitación familiar con cama individual, doble y camarotes', ARRAY['Baño privado']),
-    ('304', 'familiar', 3, 8, 30000.00, 25000.00, 20000.00, 'available', 'Habitación familiar con cama individual, doble y camarotes', ARRAY['Baño privado']),
+    ('302', 'familiar', 3, 3, 30000.00, 25000.00, 20000.00, 'available', 'Habitación familiar con cama doble y un camarote', ARRAY['Baño privado']),
+    ('303', 'familiar', 3, 6, 30000.00, 25000.00, 20000.00, 'available', 'Habitación familiar con cama dos camas doble y y dos camarotes', ARRAY['Baño privado']),
+    ('304', 'familiar', 3, 9, 30000.00, 25000.00, 20000.00, 'available', 'Habitación familiar con cama tres camas individuales y tres camarotes', ARRAY['Baño privado']),
     
     -- Fourth floor
-    ('401', 'single', 2, 2, 30000.00, 25000.00, 10000.00, 'available', 'Habitación con cama doble', ARRAY['Baño privado']),
-    ('402', 'single', 2, 2, 30000.00, 25000.00, 10000.00, 'available', 'Habitación con cama doble', ARRAY['Baño privado']),
-    ('403', 'single', 2, 2, 30000.00, 25000.00, 10000.00, 'available', 'Habitación con cama doble', ARRAY['']),
-    ('404', 'single', 2, 2, 30000.00, 25000.00, 10000.00, 'available', 'Habitación con cama doble', ARRAY['Baño privado']),
-    ('405', 'single', 2, 2, 30000.00, 25000.00, 10000.00, 'available', 'Habitación con cama doble', ARRAY['Baño privado']),
-    ('406', 'single', 2, 2, 30000.00, 25000.00, 10000.00, 'available', 'Habitación con cama doble', ARRAY['Baño privado']),
-    ('407', 'single', 2, 2, 30000.00, 25000.00, 10000.00, 'available', 'Habitación con cama doble', ARRAY['Baño privado']),
-    ('408', 'single', 2, 2, 30000.00, 25000.00, 10000.00, 'available', 'Habitación con cama doble', ARRAY['Baño privado']);
+    ('401', 'double', 4, 2, 30000.00, 25000.00, 10000.00, 'available', 'Habitación con cama doble', ARRAY['Baño privado']),
+    ('402', 'double', 4, 2, 30000.00, 25000.00, 10000.00, 'available', 'Habitación con cama doble', ARRAY['Baño privado']),
+    ('403', 'familiar', 4, 4, 30000.00, 25000.00, 10000.00, 'available', 'Habitación familiar con cama dos camas doble.', ARRAY[]::TEXT[]),
+    ('404', 'double', 4, 2, 30000.00, 25000.00, 10000.00, 'available', 'Habitación con cama doble', ARRAY['Baño privado']),
+    ('405', 'double', 4, 2, 30000.00, 25000.00, 10000.00, 'available', 'Habitación con cama doble', ARRAY['Baño privado']),
+    ('406', 'double', 4, 2, 30000.00, 25000.00, 10000.00, 'available', 'Habitación con cama doble', ARRAY['Baño privado']),
+    ('407', 'double', 4, 2, 30000.00, 25000.00, 10000.00, 'available', 'Habitación con cama doble', ARRAY['Baño privado']),
+    ('408', 'double', 4, 2, 30000.00, 25000.00, 10000.00, 'available', 'Habitación con cama doble', ARRAY['Baño privado'])
+    ON CONFLICT (room_number) DO NOTHING;
